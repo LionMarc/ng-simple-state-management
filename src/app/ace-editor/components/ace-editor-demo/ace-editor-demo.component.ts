b2bbfc8e93535171e4a0b20a8fc2bd5143235637
Marc LION <lionmarc@free.fr>
2022-10-16 14:34:00 +0200

    [ngssm-ace-editor] Notify when ace-editor is ready (#22)

--- src/app/ace-editor/components/ace-editor-demo/ace-editor-demo.component.ts
+++ src/app/ace-editor/components/ace-editor-demo/ace-editor-demo.component.ts
@@ -1,5 +1,5 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 import { NgssmAceEditorMode } from 'ngssm-ace-editor';
@@ -14,6 +14,9 @@ import { NgSsmComponent, Store } from 'ngssm-store';
 export class AceEditorDemoComponent extends NgSsmComponent {
   private readonly _updatedContent$ = new BehaviorSubject<string>('');
   private readonly _isValid$ = new BehaviorSubject<boolean>(true);
+  private readonly _isReady$ = new BehaviorSubject<boolean>(false);
+  private readonly markerIds: number[] = [];
+  private aceEditor: any;
 
   public readonly readonlyControl = new FormControl(true);
   public readonly modes: { label: string; value: NgssmAceEditorMode }[] = [
@@ -24,6 +27,7 @@ export class AceEditorDemoComponent extends NgSsmComponent {
   ];
   public readonly modeControl = new FormControl(NgssmAceEditorMode.text);
   public readonly contentControl = new FormControl('testing initial content');
+  public readonly commentPatternControl = new FormControl(undefined, Validators.required);
 
   constructor(store: Store) {
     super(store);
@@ -37,11 +41,34 @@ export class AceEditorDemoComponent extends NgSsmComponent {
     return this._isValid$.asObservable();
   }
 
+  public get isReady$(): Observable<boolean> {
+    return this._isReady$.asObservable();
+  }
+
   public onContentChanged(event: string): void {
     this._updatedContent$.next(event);
   }
 
   public onIsValidChanged(event: boolean): void {
     this._isValid$.next(event);
+  }
+
+  public onEditorReady(aceEditor: any): void {
+    this._isReady$.next(true);
+    this.aceEditor = aceEditor;
+  }
+
+  public applyCommentPattern(): void {
+    this.markerIds.forEach((id) => this.aceEditor?.getSession().removeMarker(id));
+    this.markerIds.splice(0);
+    const result = (window as any).ace.require('ace/range');
+    const pattern = new RegExp(this.commentPatternControl.value ?? '');
+    const editorContent = (this.aceEditor?.getValue() as string)?.split(/\n/);
+    editorContent.forEach((l, i) => {
+      if (pattern.test(l)) {
+        const markerId = this.aceEditor?.getSession().addMarker(new result.Range(i, 0, i, Infinity), 'ignored-row', 'fullLine', true);
+        this.markerIds.push(markerId);
+      }
+    });
   }
 }
