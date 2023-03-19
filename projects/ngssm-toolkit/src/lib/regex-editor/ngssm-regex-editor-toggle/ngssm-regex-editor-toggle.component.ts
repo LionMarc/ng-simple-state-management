a5f3c909695c4ae227a791314376f444b53d7c87
Marc LION <lionmarc@free.fr>
2023-03-19 15:35:41 +0100

    [ngssm-toolkit] Set initial focus when opening regex editor (#109)

--- projects/ngssm-toolkit/src/lib/regex-editor/ngssm-regex-editor-toggle/ngssm-regex-editor-toggle.component.ts
+++ projects/ngssm-toolkit/src/lib/regex-editor/ngssm-regex-editor-toggle/ngssm-regex-editor-toggle.component.ts
@@ -1,7 +1,7 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { OverlayModule } from '@angular/cdk/overlay';
+import { ConnectionPositionPair, OriginConnectionPosition, OverlayConnectionPosition, OverlayModule } from '@angular/cdk/overlay';
 import { MatButtonModule } from '@angular/material/button';
 import { BehaviorSubject, Observable } from 'rxjs';
 
@@ -18,6 +18,16 @@ import { NgssmRegexEditorComponent } from '../ngssm-regex-editor/ngssm-regex-edi
 export class NgssmRegexEditorToggleComponent {
   private readonly _isOpen$ = new BehaviorSubject<boolean>(false);
   private readonly _isDisabled$ = new BehaviorSubject<boolean>(false);
+  private readonly originPos: OriginConnectionPosition = {
+    originX: 'start',
+    originY: 'top'
+  };
+  private readonly overlayPos: OverlayConnectionPosition = {
+    overlayX: 'start',
+    overlayY: 'top'
+  };
+
+  public readonly positions: ConnectionPositionPair[] = [new ConnectionPositionPair(this.originPos, this.overlayPos, 0, 0)];
 
   public regexValue: string | undefined | null;
 
