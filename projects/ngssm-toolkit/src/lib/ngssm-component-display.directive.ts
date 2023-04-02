47d58261539a679b6dd531103e257a655fdff397
Marc LION <lionmarc@free.fr>
2023-04-02 09:20:06 +0200

    [ngssm-toolkit] Add custom action to be executed on component created by ngssm-component-display.directive

--- projects/ngssm-toolkit/src/lib/ngssm-component-display.directive.ts
+++ projects/ngssm-toolkit/src/lib/ngssm-component-display.directive.ts
@@ -1,16 +1,34 @@
-import { Directive, Input, ViewContainerRef } from '@angular/core';
+import { ComponentRef, Directive, Input, ViewContainerRef } from '@angular/core';
+
+export interface NgssmComponentAction {
+  (component: any): void;
+}
 
 @Directive({
   selector: '[ngssmComponentDisplay]',
   standalone: true
 })
 export class NgssmComponentDisplayDirective {
+  private componentRef?: ComponentRef<any>;
+  private componentAction?: NgssmComponentAction | null;
+
   constructor(private viewContainerRef: ViewContainerRef) {}
 
   @Input() public set ngssmComponentDisplay(value: any) {
+    this.componentRef = undefined;
     this.viewContainerRef.clear();
     if (value) {
-      this.viewContainerRef.createComponent(value);
+      this.componentRef = this.viewContainerRef.createComponent(value);
+      if (this.componentRef && this.componentAction) {
+        this.componentAction(this.componentRef.instance);
+      }
+    }
+  }
+
+  @Input() public set ngssmComponentAction(value: NgssmComponentAction | null | undefined) {
+    this.componentAction = value;
+    if (this.componentRef && this.componentAction) {
+      this.componentAction(this.componentRef.instance);
     }
   }
 }
