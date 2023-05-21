28baaa589225df7ab64555c8cc180186ae72c272
Marc LION <lionmarc@free.fr>
2023-05-21 14:45:34 +0200

    [schematics] Fix ngssm-store version installed by schematics add (#140)

--- projects/ngssm-schematics/schematics/add-ngssm/index.ts
+++ projects/ngssm-schematics/schematics/add-ngssm/index.ts
@@ -5,8 +5,8 @@ import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 function addDependencies(): Rule {
   return (host: Tree, context: SchematicContext) => {
     const dependencies: NodeDependency[] = [
-      { type: NodeDependencyType.Default, version: '^15.0.0', name: 'ngssm-toolkit' },
-      { type: NodeDependencyType.Default, version: '^15.0.0', name: 'ngssm-store' },
+      { type: NodeDependencyType.Default, version: '^16.0.0', name: 'ngssm-toolkit' },
+      { type: NodeDependencyType.Default, version: '^16.0.0', name: 'ngssm-store' },
       { type: NodeDependencyType.Default, version: '^3.1.1', name: 'immutability-helper' }
     ];
 
@@ -34,7 +34,8 @@ function updateStyles(): Rule {
       const content = tree.read(path)?.toString() ?? '';
       const insertion = `
 @import "ngssm-toolkit/styles/ngssm.scss";
-@import "ngssm-toolkit/styles/material.scss"`;
+@import "ngssm-toolkit/styles/material.scss";
+`;
 
       if (content.includes(insertion)) {
         return tree;
