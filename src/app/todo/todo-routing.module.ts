5804f220c1ef52a7375ed621723874d44e7f197f
Marc LION <lionmarc@free.fr>
2023-06-17 17:13:40 +0200

    [ngssm-remote-data] Transform ngssmReloadRemoteData into a factory (#154)

--- src/app/todo/todo-routing.module.ts
+++ src/app/todo/todo-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {
     path: 'todo-list',
     component: TodoDashboardComponent,
-    canActivate: [() => ngssmReloadRemoteData(todoItemsKey, { forceReload: false })]
+    canActivate: [ngssmReloadRemoteData(todoItemsKey, { forceReload: false })]
   }
 ];
 
