ea8e88b62badefedba24d12f2363228a4380c46f
Marc LION <lionmarc@free.fr>
2023-07-08 15:12:51 +0200

    Remove cypress (#172)
    * Remove cypress
    
    * fix angular.json

--- cypress/e2e/step_definitions/shell.ts
+++
@@ -1,9 +0,0 @@
-import { When, Then } from '@badeball/cypress-cucumber-preprocessor';
-
-When('I visit the initial project page', () => {
-  cy.visit('/');
-});
-
-Then('I should see the shell with the title {string}', (s: string) => {
-  cy.get('.ngssm-shell-header-title').contains('Demo Application');
-});
