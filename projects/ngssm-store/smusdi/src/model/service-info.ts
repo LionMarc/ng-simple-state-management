b2325464cb8f1a3a47ee65ea330f46c2206ec4a0
Marc LION <lionmarc@free.fr>
2023-10-10 11:47:49 +0200

    [ngssm-store] Update service info with custom elements info (#183)

--- projects/ngssm-store/smusdi/src/model/service-info.ts
+++ projects/ngssm-store/smusdi/src/model/service-info.ts
@@ -2,9 +2,16 @@ import { OauthInfo } from './oauth-info';
 
 export const serviceInfoKey = 'service-info';
 
+export interface ProvidedCustomElements {
+  script: string;
+  customElements: string[];
+}
+
 export interface ServiceInfo {
   serviceName: string;
   serviceVersion: string;
   environment: string;
   oauth?: OauthInfo;
+  customElements?: ProvidedCustomElements;
+  serviceUrl?: string;
 }
