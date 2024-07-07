fb2debbdb7af1ec81a23bb91972a0952a4e38aec
Marc LION <marclion7159@gmail.com>
2024-07-07 17:36:39 +0200

    Upgrade dependencies + ag-grid to 32 (#230)
    * Upgrade dependencies
    
    * Upgrade ag-grid to 32

--- projects/ngssm-ag-grid/src/lib/components/ngssm-ag-grid.directive.spec.ts
+++ projects/ngssm-ag-grid/src/lib/components/ngssm-ag-grid.directive.spec.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
+import 'ag-grid-enterprise';
 import { AgGridAngular, AgGridModule } from 'ag-grid-angular';
 import { GetRowIdParams, GridOptions } from 'ag-grid-community';
 
