082d077f329e624724e70074178cec0d6d40ba26
Marc LION <lionmarc@free.fr>
2025-03-29 12:42:58 +0100

    [ngssm-ag-grid] Allow displaying a popup from an action of the actions cell renderer

--- projects/ngssm-ag-grid/src/lib/components/ngssm-actions-cell-renderer/action-config.ts
+++ projects/ngssm-ag-grid/src/lib/components/ngssm-actions-cell-renderer/action-config.ts
@@ -1,18 +1,59 @@
-import { Signal } from '@angular/core';
+import { Signal, Type, WritableSignal } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { ICellRendererParams } from 'ag-grid-community';
 
+/**
+ * A function type that determines whether an action is disabled.
+ *
+ * @template TData - The type of the data associated with the row.
+ * @template TValue - The type of the value associated with the cell.
+ * @param params - The cell renderer parameters provided by `ag-grid`.
+ * @returns A boolean indicating whether the action is disabled.
+ */
 export type ActionDisabledFunc<TData = unknown, TValue = unknown> = (params: ICellRendererParams<TData, TValue>) => boolean;
+
+/**
+ * A function type that determines whether an action is hidden.
+ *
+ * @template TData - The type of the data associated with the row.
+ * @template TValue - The type of the value associated with the cell.
+ * @param params - The cell renderer parameters provided by `ag-grid`.
+ * @returns A boolean indicating whether the action is hidden.
+ */
 export type ActionHiddenFunc<TData = unknown, TValue = unknown> = (params: ICellRendererParams<TData, TValue>) => boolean;
+
+/**
+ * Interface for defining a popup component associated with an action.
+ *
+ * @template TData - The type of the data associated with the row.
+ * @template TValue - The type of the value associated with the cell.
+ * @template TPopupParameter - The type of the parameter passed to the popup component.
+ */
+export interface ActionPopupComponent<TData = unknown, TValue = unknown, TPopupParameter = unknown> {
+    /**
+   * An optional initialization function for the popup component.
+   *
+   * @param popupRendered - A writable signal indicating whether the popup is rendered.
+   * @param params - The cell renderer parameters provided by `ag-grid`.
+   * @param popupParameter - Additional parameters for the popup component.
+   */
+    init?: (
+      popupRendered: WritableSignal<boolean>,
+      params?: ICellRendererParams<TData, TValue>,
+      popupParameter?: TPopupParameter
+    ) => void;
+}
 
 /**
  * Configuration interface for defining actions in a cell renderer.
  *
  * @template TData - The type of the data associated with the row.
  * @template TValue - The type of the value associated with the cell.
+ * @template TPopupComponent - The type of the popup component associated with the action.
+ * @template TPopupParameter - The type of the parameter passed to the popup component.
  */
-export interface ActionConfig<TData = unknown, TValue = unknown> {
+export interface ActionConfig<TData = unknown, TValue = unknown, TPopupComponent = unknown, TPopupParameter = unknown> {
   /**
    * The CSS class used for the icon to render.
    */
@@ -52,4 +93,14 @@ export interface ActionConfig<TData = unknown, TValue = unknown> {
    * The tooltip text to display when hovering over the action element.
    */
   tooltip?: string;
+
+  /**
+   * The Angular component to render as a popup when the action is triggered.
+   */
+  popupComponent?: Type<TPopupComponent>;
+
+  /**
+   * Additional parameters to pass to the popup component.
+   */
+  popupParameter?: TPopupParameter;
 }
--- projects/ngssm-ag-grid/src/lib/components/ngssm-actions-cell-renderer/ngssm-actions-cell-renderer.component.spec.ts
+++ projects/ngssm-ag-grid/src/lib/components/ngssm-actions-cell-renderer/ngssm-actions-cell-renderer.component.spec.ts
@@ -1,29 +1,58 @@
-import { signal } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatCardHarness } from '@angular/material/card/testing';
+import { MatCard, MatCardActions, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
 import { BehaviorSubject } from 'rxjs';
 
 import { ICellRendererParams } from 'ag-grid-community';
 
 import { NgssmActionsCellRendererParams } from './ngssm-actions-cell-renderer-params';
 import { NgssmActionsCellRendererComponent } from './ngssm-actions-cell-renderer.component';
+import { ActionPopupComponent } from './action-config';
 
 interface TestingData {
   value: number;
+}
+
+@Component({
+  selector: 'ngssm-testing-popup',
+  template: `
+    <mat-card class="flex-column-stretch">
+      <mat-card-header>
+        <mat-card-title>{{ title() }}</mat-card-title>
+      </mat-card-header>
+      <mat-card-actions>
+        <button mat-button (click)="isPopupOpen.set(false)">Close</button>
+      </mat-card-actions>
+    </mat-card>
+  `,
+  imports: [MatCard, MatCardHeader, MatCardActions, MatCardTitle, MatButton]
+})
+class TestingPoupuComponent implements ActionPopupComponent<TestingData> {
+  public readonly title = signal<string>('Popup Title');
+  public isPopupOpen = signal(false);
+
+  public init(popupRendered: WritableSignal<boolean>, params?: ICellRendererParams<TestingData, unknown>, popupParameter?: unknown): void {
+    this.isPopupOpen = popupRendered;
+    this.title.set(`${popupParameter}: ${params?.data?.value}`);
+  }
 }
 
 describe('NgssmActionsCellRendererComponent', () => {
   let component: NgssmActionsCellRendererComponent;
   let fixture: ComponentFixture<NgssmActionsCellRendererComponent>;
   let loader: HarnessLoader;
+  let rootLoader: HarnessLoader;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [NgssmActionsCellRendererComponent],
-      teardown: { destroyAfterEach: true }
+      imports: [NgssmActionsCellRendererComponent, TestingPoupuComponent],
+      teardown: { destroyAfterEach: false }
     });
 
     fixture = TestBed.createComponent(NgssmActionsCellRendererComponent);
@@ -31,6 +60,7 @@ describe('NgssmActionsCellRendererComponent', () => {
     fixture.nativeElement.style['min-height'] = '300px';
     fixture.nativeElement.style['margin-top'] = '20px';
     loader = TestbedHarnessEnvironment.loader(fixture);
+    rootLoader= TestbedHarnessEnvironment.documentRootLoader(fixture);
     fixture.detectChanges();
   });
 
@@ -324,6 +354,56 @@ describe('NgssmActionsCellRendererComponent', () => {
       const element = fixture.debugElement.query(By.css('#action_0'));
 
       expect(element).toBeTruthy();
+    });
+  });
+
+  describe('with a custom popup component', () => {
+    let cellRendererParams: NgssmActionsCellRendererParams<TestingData> | ICellRendererParams = {
+      actions: []
+    };
+
+    beforeEach(async () => {
+      cellRendererParams = {
+        actions: [
+          {
+            cssClass: 'fa-solid fa-trash-can',
+            popupComponent: TestingPoupuComponent,
+            popupParameter: 'Testing'
+          }
+        ],
+        data: {
+          value: 6
+        }
+      };
+
+      component.agInit({
+        ...cellRendererParams
+      } as unknown as ICellRendererParams);
+
+      fixture.detectChanges();
+
+      const element = await loader.getHarness(MatButtonHarness.with({ selector: '#action_0' }));
+
+      await element.click();
+    });
+
+    it(`should render the popup component`,  () => {
+      const popup = document.querySelector('ngssm-testing-popup');
+      expect(popup).toBeTruthy();
+    });
+
+    it(`should render the title with the current row data and the popup parameter`,async  () => {
+      const popup = await rootLoader.getHarness(MatCardHarness.with({ selector: 'ngssm-testing-popup mat-card' }));
+      const title = await popup.getTitleText();
+      expect(title).toBe('Testing: 6');
+    });
+
+    it(`should hide the popup when clicking on No button`,async  () => {
+      const popup = await rootLoader.getHarness(MatCardHarness.with({ selector: 'ngssm-testing-popup mat-card' }));
+      const button = await popup.getHarness(MatButtonHarness.with({ text: 'Close' }));
+      await button.click();
+      const popup2 = document.querySelector('ngssm-testing-popup');
+      expect(popup2).toBeFalsy();
     });
   });
 });
