54e4a68167724e9c3dd13c6d917967f8f7b8f704
Marc LION <marclion7159@gmail.com>
2025-04-10 21:13:13 +0200

    [ngssm-ag-grid] NgssmBaseCellEditor must not extend anymore NgssmComponent (#277)

--- projects/ngssm-ag-grid/src/lib/components/ngssm-base-cell-editor.ts
+++ projects/ngssm-ag-grid/src/lib/components/ngssm-base-cell-editor.ts
@@ -1,14 +1,8 @@
 import { ICellEditorAngularComp } from 'ag-grid-angular';
 import { ICellEditorParams } from 'ag-grid-community';
 
-import { NgSsmComponent, Store } from 'ngssm-store';
-
-export abstract class NgssmBaseCellEditor extends NgSsmComponent implements ICellEditorAngularComp {
+export abstract class NgssmBaseCellEditor implements ICellEditorAngularComp {
   protected params?: ICellEditorParams;
-
-  constructor(store: Store) {
-    super(store);
-  }
 
   public agInit(params: ICellEditorParams): void {
     this.params = params;
