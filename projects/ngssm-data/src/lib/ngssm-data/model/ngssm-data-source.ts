c2b8afa60ccc424f04a7863f13ab4789cc0e9370
Marc LION <marclion7159@gmail.com>
2025-05-17 14:57:34 +0200

    [ngssm-data] Should be able to link a list of data sources to one source (#297)
    * [ngssm-data] Should be able to link a list of data sources to one source
    
    * Fix typo and add a test
    
    * Fix typo + add comment

--- projects/ngssm-data/src/lib/ngssm-data/model/ngssm-data-source.ts
+++ projects/ngssm-data/src/lib/ngssm-data/model/ngssm-data-source.ts
@@ -19,39 +19,43 @@ export type NgssmAdditionalPropertyLoading<TData = unknown> = (
 ) => Observable<TData>;
 
 // Interface defining the structure of a data source
-export interface NgssmDataSource<TData = unknown, TParameter = unknown, TAdditionProperty = unknown> {
+// Be careful when using linkedToDataSource or linkedDataSources because no check is done to validate that there is no 
+// circular dependencies (A => B => C => A, for example).
+export interface NgssmDataSource<TData = unknown, TParameter = unknown, TAdditionalProperty = unknown> {
   key: string; // Unique identifier for the data source
   dataLifetimeInSeconds?: number; // Optional lifetime for cached data
   dataLoadingFunc: NgssmDataLoading<TData, TParameter>; // Function to load data
-  additionalPropertyLoadingFunc?: NgssmAdditionalPropertyLoading<TAdditionProperty>; // Optional function to load additional properties
+  additionalPropertyLoadingFunc?: NgssmAdditionalPropertyLoading<TAdditionalProperty>; // Optional function to load additional properties
   initialParameter?: TParameter; // Optional initial parameter for the data source
   initialParameterInvalid?: boolean; // Flag indicating if the initial parameter is invalid,
-  linkedToDataSource?: string; // If target data source valued is updated, a reload is made for this data source.
+  linkedToDataSource?: string; // If target data source valued is updated, a reload is made for this data source. This is used to force updating this data source.
+  linkedDataSources?: string[]; // If current source is updated, all the linked data sources are reloaded. This is used to force updating the other data sources.
 }
 
 // Injection token for registering data sources
 export const NGSSM_DATA_SOURCE = new InjectionToken<NgssmDataSource>('NGSSM_DATA_SOURCE');
 
 // Optional parameters when registering a data source
-export interface NgssmDataSourceProvideOptions<TParameter = unknown, TAdditionProperty = unknown> {
+export interface NgssmDataSourceProvideOptions<TParameter = unknown, TAdditionalProperty = unknown> {
   dataLifetimeInSeconds?: number; // Optional data lifetime
   initialParameter?: TParameter; // Optional initial parameter
   initialParameterInvalid?: boolean; // Optional flag for invalid initial parameter
-  additionalPropertyLoadingFunc?: NgssmAdditionalPropertyLoading<TAdditionProperty>; // Optional function to load additional properties,
+  additionalPropertyLoadingFunc?: NgssmAdditionalPropertyLoading<TAdditionalProperty>; // Optional function to load additional properties,
   linkedToDataSource?: string; // If target data source valued is updated, a reload is made for this data source.
+  linkedDataSources?: string[]; // If current source is updated, all the linked data sources are reloaded.
 }
 
 // Function to provide a data source as an environment provider
-export const provideNgssmDataSource = <TData = unknown, TParameter = unknown, TAdditionProperty = unknown>(
+export const provideNgssmDataSource = <TData = unknown, TParameter = unknown, TAdditionalProperty = unknown>(
   key: string, // Unique key for the data source
   loadingFunc: NgssmDataLoading<TData, TParameter>, // Function to load data
-  options?: NgssmDataSourceProvideOptions<TParameter, TAdditionProperty> // Optional configuration options
+  options?: NgssmDataSourceProvideOptions<TParameter, TAdditionalProperty> // Optional configuration options
 ): EnvironmentProviders => {
   return makeEnvironmentProviders([
     {
       provide: NGSSM_DATA_SOURCE,
       useFactory: () => {
-        const dataSource: NgssmDataSource<TData, TParameter> = {
+        const dataSource: NgssmDataSource<TData, TParameter, TAdditionalProperty> = {
           key,
           dataLifetimeInSeconds: options?.dataLifetimeInSeconds,
           dataLoadingFunc: loadingFunc,
@@ -68,6 +72,10 @@ export const provideNgssmDataSource = <TData = unknown, TParameter = unknown, TA
 
         if (options?.linkedToDataSource) {
           dataSource.linkedToDataSource = options?.linkedToDataSource;
+        }
+
+        if (options?.linkedDataSources) {
+          dataSource.linkedDataSources = options?.linkedDataSources;
         }
 
         return dataSource;
