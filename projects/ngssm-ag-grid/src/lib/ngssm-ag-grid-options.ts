2e66c442de9abca896c27f0767072c2324e7c133
Marc LION <marclion7159@gmail.com>
2025-08-03 09:45:49 +0200

    [ag-grid] Add options to provideNgssmAgGrid function (#334)

--- projects/ngssm-ag-grid/src/lib/ngssm-ag-grid-options.ts
+++ projects/ngssm-ag-grid/src/lib/ngssm-ag-grid-options.ts
@@ -1,6 +1,12 @@
 import { InjectionToken } from '@angular/core';
 import { ColDef, StatusPanelDef } from 'ag-grid-community';
 
+/**
+ * Configuration options for NgssmAgGrid.
+ * - theme: The CSS theme to use for the grid.
+ * - statusBar: Optional status bar configuration for AG Grid.
+ * - defaultColDef: Optional default column definition for all columns.
+ */
 export class NgssmAgGridOptions {
   theme = 'ag-theme-material';
   statusBar:
@@ -11,4 +17,9 @@ export class NgssmAgGridOptions {
   defaultColDef: ColDef | undefined = undefined;
 }
 
+/**
+ * Injection token for providing NgssmAgGridOptions in Angular DI.
+ *
+ * Provide options through the provideNgssmAgGrid function instead of using this token.
+ */
 export const NGSSM_AG_GRID_OPTIONS = new InjectionToken<NgssmAgGridOptions>('NGSSM_AG_GRID_OPTIONS');
