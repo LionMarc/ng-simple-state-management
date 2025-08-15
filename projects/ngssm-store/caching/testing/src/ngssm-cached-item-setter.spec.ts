440951632e0417080b87719295ffc5888404f3a8
Marc LION <lionmarc@free.fr>
2025-08-15 07:32:51 +0200

    [caching-testing] Fix typo

--- projects/ngssm-store/caching/testing/src/ngssm-cached-item-setter.spec.ts
+++ projects/ngssm-store/caching/testing/src/ngssm-cached-item-setter.spec.ts
@@ -18,14 +18,14 @@ describe('NgssmCachedItemSetter', () => {
   });
 
   it(`should update the status of a data source`, () => {
-    TestBed.inject(NgssmCachedItemSetter).setCacheItemStatus(key, CachedItemStatus.loading);
+    TestBed.inject(NgssmCachedItemSetter).setCachedItemStatus(key, CachedItemStatus.loading);
 
     const store = TestBed.inject(Store);
     expect(selectNgssmCachedItem(store.state(), key)?.status).toEqual(CachedItemStatus.loading);
   });
 
   it(`should update the value of a data source`, () => {
-    TestBed.inject(NgssmCachedItemSetter).setCacheItemValue(key, { label: 'testing update' });
+    TestBed.inject(NgssmCachedItemSetter).setCachedItemValue(key, { label: 'testing update' });
 
     const store = TestBed.inject(Store);
     expect(selectNgssmCachedItem(store.state(), key)?.item).toEqual({ label: 'testing update' });
