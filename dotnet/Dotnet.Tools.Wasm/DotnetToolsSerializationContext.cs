a4628156d02b12595be3e47b99d5a73b1b08d3d8
Marc LION <marclion7159@gmail.com>
2025-09-14 09:11:54 +0200

    [toolkit] Remove regex editor and dotnet code (moved into **-regex-tool repos) (#354)
    * [toolkit] Remove regex editor and dotnet code (moved into **-regex-tool repos)
    
    * Empty

--- dotnet/Dotnet.Tools.Wasm/DotnetToolsSerializationContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Dotnet.Tools.Wasm;
-
-[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
-[JsonSerializable(typeof(RegexValidationResult))]
-public partial class DotnetToolsSerializationContext : JsonSerializerContext
-{
-}
