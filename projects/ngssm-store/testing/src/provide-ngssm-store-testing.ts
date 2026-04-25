d8de837dc17cc9376bfa70a817e7933eeabd982e
Marc LION <lionmarc@free.fr>
2026-04-25 13:27:36 +0200

    [store.testing] Register also the StoreMock with the same value as the Store

--- projects/ngssm-store/testing/src/provide-ngssm-store-testing.ts
+++ projects/ngssm-store/testing/src/provide-ngssm-store-testing.ts
@@ -11,14 +11,19 @@ import { StoreMock } from './store-mock';
  * with a `StoreMock` instance, allowing for isolated and controlled testing of components or services
  * that depend on the `Store`.
  *
+ * This function registers the `Store` and the `StoreMock` with the same value.
+ *
  * @param action Optional callback function that receives the mock `Store` instance. Can be used to configure
  *               or manipulate the mock store before it is provided to the testing environment.
  * @returns The environment providers configured with the `StoreMock`.
  */
-export const provideNgssmStoreTesting = (action?: (store: Store) => void): EnvironmentProviders => {
+export const provideNgssmStoreTesting = (action?: (store: StoreMock) => void): EnvironmentProviders => {
   const store = new StoreMock({});
   if (action) {
-    action(store as unknown as Store);
+    action(store);
   }
-  return makeEnvironmentProviders([{ provide: Store, useValue: store }]);
+  return makeEnvironmentProviders([
+    { provide: Store, useValue: store },
+    { provide: StoreMock, useValue: store }
+  ]);
 };
